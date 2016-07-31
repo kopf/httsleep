6cea9448c2ab0e9a2eee24078bd9936d2282a47f
Aengus Walton <ventolin@gmail.com>
2016-07-31 14:41:17 +0200

    Add tests for retries. Switch to httpretty instead of responses, since it can handle a chain of responses better

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,5 +1,5 @@
 import mock
-import responses
+import httpretty
 import pytest
 
 from httsleep.main import HttSleep, HttSleepError
@@ -7,35 +7,58 @@ from httsleep.main import HttSleep, HttSleepError
 URL = 'http://example.com'
 
 
-@pytest.fixture(scope='function')
-def mock_sleep():
-    return mock.Mock('httsleep.main.sleep')
-
-
-@responses.activate
-def test_run_success(mock_sleep):
+@httpretty.activate
+def test_run_success():
     """Should return response when a success criteria has been reached"""
-    responses.add(responses.GET, URL, body='<html></html>', status=200)
-    httsleep = HttSleep(URL, {'status_code': 200})
-    resp = httsleep.run()
-    assert resp.status_code == 200
-    assert not mock_sleep.called
+    httpretty.register_uri(httpretty.GET, URL, body='<html></html>', status=200)
+    with mock.patch('httsleep.main.sleep') as mock_sleep:
+        httsleep = HttSleep(URL, {'status_code': 200})
+        resp = httsleep.run()
+        assert resp.status_code == 200
+        assert not mock_sleep.called
 
 
-
-@responses.activate
+@httpretty.activate
 def test_run_error():
     """Should raise an HttSleepError when a failure criteria has been reached"""
-    responses.add(responses.GET, URL, body='<html></html>', status=400)
+    httpretty.register_uri(httpretty.GET, URL, body='<html></html>', status=400)
     httsleep = HttSleep(URL, {'status_code': 200}, error={'status_code': 400})
     with pytest.raises(HttSleepError):
-        resp = httsleep.run()
+        httsleep.run()
 
 
-@responses.activate
+@httpretty.activate
 def test_run_success_error():
     """Make sure failure criteria takes precedence over success criteria (if httsleep is being used incorrectly)"""
-    responses.add(responses.GET, URL, body='', status=200)
+    httpretty.register_uri(httpretty.GET, URL, body='', status=200)
     httsleep = HttSleep(URL, {'status_code': 200}, error={'text': ''})
     with pytest.raises(HttSleepError):
-        resp = httsleep.run()
+        httsleep.run()
+
+
+@httpretty.activate
+def test_run_retries():
+    """Should retry until a success condition is reached"""
+    responses = [httpretty.Response(body="Internal Server Error", status=500),
+                 httpretty.Response(body="Internal Server Error", status=500),
+                 httpretty.Response(body="<html></html>", status=200)]
+    httpretty.register_uri(httpretty.GET, URL, responses=responses)
+    with mock.patch('httsleep.main.sleep') as mock_sleep:
+        resp = HttSleep(URL, {'status_code': 200}).run()
+        assert mock_sleep.called
+        assert mock_sleep.call_count == 2
+    assert resp.status_code == 200
+    assert resp.text == '<html></html>'
+
+
+@httpretty.activate
+def test_run_max_retries():
+    """Should raise an exception when max_retries is reached"""
+    responses = [httpretty.Response(body="Internal Server Error", status=500),
+                 httpretty.Response(body="Internal Server Error", status=500),
+                 httpretty.Response(body="Internal Server Error", status=500)]
+    httpretty.register_uri(httpretty.GET, URL, responses=responses)
+    with mock.patch('httsleep.main.sleep') as mock_sleep:
+        httsleep = HttSleep(URL, {'status_code': 200}, max_retries=2)
+        with pytest.raises(StopIteration):
+            httsleep.run()
