4a1c744ef8912f4d0823bd3100805171475c7eac
Patrick Muehlbauer <patrick.muehlbauer@blue-yonder.com>
2016-08-08 09:34:45 +0200

    Add setup.py and setup.cfg to make httsleep installable.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+"""
+from setuptools import setup, find_packages
+
+
+def setup_package():
+    setup(name='httsleep',
+          description='A python library for polling HTTP endpoints - batteries included!',
+          author='Aengus Walton',
+          author_email='',
+          url='https://github.com/kopf',
+          packages=find_packages(exclude=['tests', 'test']),
+          classifiers=[
+              'Development Status :: 4 - Beta',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 2.7'
+          ],
+          zip_safe=False,
+          include_package_data=True,
+          setup_requires=['setuptools_scm'],
+          install_requires=['requests', 'jsonpath-rw'],
+          use_scm_version=True)
+
+
+if __name__ == "__main__":
+    setup_package()
