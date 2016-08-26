bb896386921e618526514913d01cb69ba95ded75
Aengus Walton <Aengus.Walton@blue-yonder.com>
2016-08-26 11:29:13 +0200

    Fix setup.py, add changelog

---
+++ CHANGELOG.rst
@@ -0,0 +1,13 @@
+httsleep Changelog
+==================
+
+Version 0.1.1
+-------------
+
+* Added CHANGELOG
+* Fix setup.py
+
+Version 0.1
+-----------
+
+* Initial release
--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ def setup_package():
           classifiers=[
               'Development Status :: 4 - Beta',
               'Programming Language :: Python',
-              'Programming Language :: Python :: 2.7'
-              'Programming Language :: Python :: 3.4'
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3.4',
               'Programming Language :: Python :: 3.5'
           ],
           zip_safe=False,
