23a0309e0b206fb9e62db8ef8edd027413343221
Aengus Walton <Aengus.Walton@blue-yonder.com>
2016-09-07 17:01:27 +0200

    Use sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import pkg_resources
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -123,7 +124,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -138,7 +139,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
